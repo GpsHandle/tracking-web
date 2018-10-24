df45c4654c0ddfd219fbf156ed4282b2ef452955
beou <hoaivubk@gmail.com>
2018-10-24 23:54:07 +0700

    added new sys-admin module

--- src/app/sys-admin/sys-admin-routing.module.ts
+++ src/app/sys-admin/sys-admin-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from 'app/sys-admin/login/login.component';
 import { LogoutComponent } from 'app/sys-admin/logout/logout.component';
+import { SysAdminGuard } from 'app/guards/sys-admin.guard';
 
 const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -9,7 +10,9 @@ const routes: Routes = [
     { path: 'logout', component: LogoutComponent },
     {
         path: 'sys',
-        loadChildren: 'app/sys-admin/sys-dashboard/sys-dashboard.module#SysDashboardModule'
+        loadChildren: 'app/sys-admin/sys-dashboard/sys-dashboard.module#SysDashboardModule',
+        canLoad: [SysAdminGuard],
+        canActivate: [SysAdminGuard]
     }
 ];
 
