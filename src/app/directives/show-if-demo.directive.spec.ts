146dff5c0e020b96085ff3a5acbf339b4ec608de
beou <hoaivubk@gmail.com>
2018-12-20 09:23:21 +0700

    added demo component

---
+++ src/app/directives/show-if-demo.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ShowIfDemoDirective } from './show-if-demo.directive';
+
+describe('ShowIfDemoDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ShowIfDemoDirective();
+    expect(directive).toBeTruthy();
+  });
+});
