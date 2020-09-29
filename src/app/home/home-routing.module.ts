7824caa413acb525e4d67482bd4590e087f7b4c4
duvu <hoaivubk@gmail.com>
2020-09-29 16:56:19 +0700

    added promote pages @web

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,12 +6,15 @@ import {AboutComponent} from "./about/about.component";
 import {HelpComponent} from "./help/help.component";
 import {TosComponent} from "./tos/tos.component";
 import {PrivacyPolicyComponent} from "./privacy-policy/privacy-policy.component";
+import {HomeContentComponent} from "./home-content/home-content.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: 'pages/main', pathMatch: 'full' },
     {
-      path: '',
+      path: 'pages',
       component: HomeComponent,
       children: [
+        { path: 'main', component: HomeContentComponent, pathMatch: 'full'},
         { path: 'about', component: AboutComponent, pathMatch: 'full'},
         { path: 'privacy', component: PrivacyPolicyComponent, pathMatch: 'full'},
         { path: 'tos', component: TosComponent, pathMatch: 'full'},
