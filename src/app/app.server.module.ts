2360f04e817bfba923937dde5a9284163ba28872
duvu <hoaivubk@gmail.com>
2020-10-10 08:01:09 +0700

    upgrade angular 10

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,18 +3,16 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 import { FlexLayoutServerModule } from '@angular/flex-layout/server';
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {UniversalInterceptor} from "./interceptors/universal-interceptor";
 
 @NgModule({
     imports: [
-        AppModule,
-        ServerModule,
-        ModuleMapLoaderModule,
-        FlexLayoutServerModule
-    ],
+    AppModule,
+    ServerModule,
+    FlexLayoutServerModule
+],
     providers: [{
         provide: HTTP_INTERCEPTORS,
         useClass: UniversalInterceptor,
