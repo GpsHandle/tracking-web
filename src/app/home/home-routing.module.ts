0e22de3b56f794e2e8313fc491b75e0daf82e26d
duvu <hoaivubk@gmail.com>
2020-10-10 07:28:15 +0700

    upgrade angular 10

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -10,17 +10,19 @@ import {HomeContentComponent} from "./home-content/home-content.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'pages/main', pathMatch: 'full' },
-    {
-      path: 'pages',
-      component: HomeComponent,
-      children: [
-        { path: 'main', component: HomeContentComponent, pathMatch: 'full'},
-        { path: 'about', component: AboutComponent, pathMatch: 'full'},
-        { path: 'privacy', component: PrivacyPolicyComponent, pathMatch: 'full'},
-        { path: 'tos', component: TosComponent, pathMatch: 'full'},
-        { path: 'help', component: HelpComponent, pathMatch: 'full'},
-      ]
-    }];
+  {
+    path: 'pages',
+    component: HomeComponent,
+    children: [
+      { path: 'main', component: HomeContentComponent, pathMatch: 'full'},
+      { path: 'about', component: AboutComponent, pathMatch: 'full'},
+      { path: 'privacy', component: PrivacyPolicyComponent, pathMatch: 'full'},
+      { path: 'tos', component: TosComponent, pathMatch: 'full'},
+      { path: 'help', component: HelpComponent, pathMatch: 'full'},
+    ]
+  },
+  { path: '**', redirectTo: '/pages/main', pathMatch: 'full' }
+    ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
